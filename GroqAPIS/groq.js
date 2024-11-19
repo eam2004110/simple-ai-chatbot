56f91bf5f48e8ed7401f928f3fb3de0b22a3c70d
eam2004110 <e.a.m200411x@gmail.com>
2024-11-19 23:17:36 +0100

    up

--- GroqAPIS/groq.js
+++ GroqAPIS/groq.js
@@ -6,8 +6,8 @@ export default async function generateGroqCompletion(
   prompt = "Hello",
   model = "llama3-groq-70b-8192-tool-use-preview",
   systemInstructions = `
--Always structure the answer and style it on html, good colors ,text colors, colors depending on the case, good fonts , and large font size, and choose dark mode style or light depending on the case ,then pay attention to text colors, they must be seen on dark mode, and on light mode, the mode must be seen on backgrounds and styles , the content must be vertical so do not write many block elements, return that html as output;
--pay attention the UI mode must be opposite of text mode if not : so the text will not be seen;
+-Always structure the answer and style it on html, good colors ,text colors, colors depending on the case, good fonts , and large font size, the content must be vertical so do not write many block elements, return that html as output;
+-text colors must be readable on their backgrounds;
 -the html must be a div that centers its content, put text in center only when it is good, else if only for sub-titles do not;
 -the html must behave as a simple website , the content is vertical not horizontal;
 -if the generated html includes images: do not set "src" attribute, only add the "alt" attribute (each image set for it an appropriate "alt" value, "alt" must be tokens that can we use as a query on image providers serching) ,and set the attribute : searchImage="true" , and on that case of adding images : add a script tag on the generated html , with src attribute : src="searchImage.js", then add another script tag that calls the function : searchImage();
